e0d9b627c852e89bc41be19edf252bc2a1686248
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-07-26 09:52:03 +0300

    refactor

--- src/main/java/by/htp/ex/bean/NewUserInfo.java
+++ src/main/java/by/htp/ex/bean/NewUserInfo.java
@@ -1,60 +1,64 @@
 package by.htp.ex.bean;
 
 public class NewUserInfo {
-	private String name;
-	private String surname;
-	private String email;
-	private String password;
-	private String birthday;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String birthday;
 
-	public NewUserInfo(){}
-	
-	public String getBirthday() {
-		return birthday;
-	}
+    public NewUserInfo() {
+    }
 
-	public void setBirthday(String birthday) {
-		this.birthday = birthday;
-	}
+    public NewUserInfo(String name, String surname, String email, String password, String birthday) {
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getBirthday() {
+        return birthday;
+    }
 
-	@Override
-	public String toString() {
-		return "User [name=" + name + ", surname=" + surname + ", email=" + email + ", password=" + password
-				+ ", birthday=" + birthday + "]";
-	}
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getSurname() {
-		return surname;
-	}
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", surname=" + surname + ", email=" + email + ", password=" + password
+                + ", birthday=" + birthday + "]";
+    }
 
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getSurname() {
+        return surname;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
 }
 
--- src/main/java/by/htp/ex/controller/impl/DoRegistration.java
+++ src/main/java/by/htp/ex/controller/impl/DoRegistration.java
@@ -1,7 +1,5 @@
 package by.htp.ex.controller.impl;
 
-import java.io.IOException;
-
 import by.htp.ex.bean.NewUserInfo;
 import by.htp.ex.controller.Command;
 import by.htp.ex.controller.JspPageName;
@@ -13,44 +11,37 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
 public class DoRegistration implements Command {
+    UserService service = ServiceProvider.getInstance().getUserService();
 
-	
-		UserService service = ServiceProvider.getInstance().getUserService();
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter(RequestParameterName.NAME);
+        String surname = request.getParameter(RequestParameterName.SURNAME);
+        String email = request.getParameter(RequestParameterName.EMAIL);
+        String password = request.getParameter(RequestParameterName.PASSWORD);
+        String birthday = request.getParameter(RequestParameterName.BIRTHDAY);
+        if (email == null || password == null || name == null || surname == null || birthday == null) {
+            request.getRequestDispatcher(JspPageName.BASELAYOUT_PAGE).forward(request, response);
+        }
+        NewUserInfo user = new NewUserInfo(name, surname, email, password, birthday);
 
-	    @Override
-	    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	        String name = request.getParameter(RequestParameterName.NAME);
-	        String surname = request.getParameter(RequestParameterName.SURNAME);
-	        String email = request.getParameter(RequestParameterName.EMAIL);
-	        String password = request.getParameter(RequestParameterName.PASSWORD);
-	        String birthday = request.getParameter(RequestParameterName.BIRTHDAY);
-	        if (email == null || password == null || name == null || surname == null || birthday == null) {
-	            request.getRequestDispatcher(JspPageName.BASELAYOUT_PAGE).forward(request, response);
-	        }
-	        NewUserInfo user = new NewUserInfo();
-	        user.setName(name);
-	        user.setSurname(surname);
-	        user.setEmail(email);
-	        user.setPassword(password);
-	        user.setBirthday(birthday);
-	        try {
-	            boolean result = service.registration(user);
-	            if (result) {
-	            	response.sendRedirect("index.jsp");
-	            } else {
-	                request.setAttribute("massage", request.getParameter("email") + " is already exist");
-	                request.setAttribute("reg", "reg");
-	                request.getRequestDispatcher(JspPageName.BASELAYOUT_PAGE).forward(request, response);
-	            }
-	        } catch (ServiceException e) {
-	        	request.setAttribute("reg", "reg");
-	            request.getRequestDispatcher(JspPageName.BASELAYOUT_PAGE).forward(request, response);
-	        }
-	    }
-		
-		
-		
-	
+        try {
+            boolean result = service.registration(user);
+            if (result) {
+                response.sendRedirect("index.jsp");
+            } else {
+                request.setAttribute("massage", request.getParameter("email") + " is already exist");
+                request.setAttribute("reg", "reg");
+                request.getRequestDispatcher(JspPageName.BASELAYOUT_PAGE).forward(request, response);
+            }
+        } catch (ServiceException e) {
+            request.setAttribute("reg", "reg");
+            request.getRequestDispatcher(JspPageName.BASELAYOUT_PAGE).forward(request, response);
+        }
+    }
+
 
 }
