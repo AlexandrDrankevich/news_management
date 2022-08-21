dc6b3c0147187ce57ad1621b46128491e47e2f58
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-08-21 09:28:54 +0300

    1

--- src/main/java/by/htp/ex/controller/impl/DoSIgnIn.java
+++ src/main/java/by/htp/ex/controller/impl/DoSIgnIn.java
@@ -16,31 +16,29 @@ import java.io.IOException;
 
 public class DoSIgnIn implements Command {
 
-    private final UserService service = ServiceProvider.getInstance().getUserService();
-    private static final Logger log = LogManager.getLogger(DoSIgnIn.class);
+	private final UserService service = ServiceProvider.getInstance().getUserService();
+	private static final Logger log = LogManager.getLogger(DoSIgnIn.class);
 
-    @Override
-    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String go_to_news_list = "controller?command=go_to_news_list";
-        String login = request.getParameter(RequestParameterName.LOGIN);
-        String password = request.getParameter(RequestParameterName.PASSWORD);
-        try {
-            String role = service.signIn(login, password);
-            if (!role.equals("guest")) {
-                request.getSession(true).setAttribute("login", login);
-                request.getSession(true).setAttribute("user", "active");
-                request.getSession(true).setAttribute("role", role);
-                request.getSession(true).setAttribute("url", go_to_news_list);
-                response.sendRedirect(go_to_news_list);
-            } else {
-                request.getSession(true).setAttribute("user", "not active");
-                request.getSession(true).setAttribute("url", go_to_news_list);
-                response.sendRedirect("controller?command=go_to_base_page&AuthenticationError=wrong login or password");
-            }
-        } catch (ServiceException e) {
-            log.error(e);
-            response.sendRedirect(JspPageName.INDEX_PAGE);
-        }
-    }
+		String login = request.getParameter(RequestParameterName.LOGIN);
+		String password = request.getParameter(RequestParameterName.PASSWORD);
+		try {
+			String role = service.signIn(login, password);
+			if (!role.equals("guest")) {
+				request.getSession(true).setAttribute("login", login);
+				request.getSession(true).setAttribute("user", "active");
+				request.getSession(true).setAttribute("role", role);
+				response.sendRedirect("controller?command=go_to_news_list");
+			} else {
+				request.getSession(true).setAttribute("user", "not active");
+				request.getSession(true).setAttribute("url", "controller?command=go_to_base_page");
+				response.sendRedirect("controller?command=go_to_base_page&AuthenticationError=wrong login or password");
+			}
+		} catch (ServiceException e) {
+			log.error(e);
+			response.sendRedirect(JspPageName.INDEX_PAGE);
+		}
+	}
 }
